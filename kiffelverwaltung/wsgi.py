d45bcfbfde6d7e97df4e5217dfecc2ede715172d
Johannes Lauinger <johannes@lauinger-it.de>
2016-03-08 21:09:53 +0100

    creates django project with django rest framework

---
+++ kiffelverwaltung/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kiffelverwaltung project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kiffelverwaltung.settings")
+
+application = get_wsgi_application()
