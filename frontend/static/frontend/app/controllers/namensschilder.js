403cb490a41247cab51c72869805821c36d0f763
Johannes Lauinger <johannes@lauinger-it.de>
2016-03-10 01:50:33 +0100

    adds frontend actions for generating nametags

--- frontend/static/frontend/app/controllers/namensschilder.js
+++ frontend/static/frontend/app/controllers/namensschilder.js
@@ -3,9 +3,23 @@
   "use strict";
   var app = angular.module('kiffel');
 
-  app.controller('namensschilderCtrl', ['$scope', 'Restangular', 'toolbarService', function($scope, Restangular, toolbarService) {
+  app.controller('namensschilderCtrl', ['$scope', '$window', 'toolbarService', function($scope, $window, toolbarService) {
 
-    toolbarService.title('Namensschilder erstellen')
+    toolbarService.title('Namensschilder erstellen');
+
+    $scope.filterChoice = 'all';
+
+    $scope.generate = function() {
+      var search = '';
+      if ($scope.filterChoice === 'orga') {
+        search = '?ist_orga=True';
+      } else if ($scope.filterChoice === 'nickname') {
+        search = '?nickname=' + encodeURIComponent($scope.filterValue);
+      } else if ($scope.filterChoice === 'hochschule') {
+        search = '?hochschule=' + encodeURIComponent($scope.filterValue);
+      }
+      $window.open('api/v1/nametags/' + search, '_blank');
+    };
 
   }]);
 
