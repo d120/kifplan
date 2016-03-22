f7c14a2fbafb40b801b471ef33c48f45a994b5f9
Johannes Lauinger <johannes@lauinger-it.de>
2016-03-23 00:45:14 +0100

    adds simple model for AK based on wiki fields

--- aks/admin.py
+++ aks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from aks.models import AK
+
+@admin.register(AK)
+class AKAdmin(admin.ModelAdmin):
+    # admin list table view
+    list_display = ['titel', 'leiter', 'anzahl', 'wann', 'dauer']
+    list_display_links = ['titel']
