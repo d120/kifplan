cb2197b2938ec8927ac10bafb01857d4e386c93c
Johannes Lauinger <johannes@lauinger-it.de>
2016-03-22 23:56:00 +0100

    adds initial model representation for a guest account, and adds it to the admin dashboard

--- eduroam/admin.py
+++ eduroam/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from eduroam.models import GuestAccount
+
+@admin.register(GuestAccount)
+class GuestAccountAdmin(admin.ModelAdmin):
+    # admin list table view
+    list_display = ['login', 'password', 'vorname', 'nachname', 'vergeben', 'vergeben_am']
+    list_display_links = ['login']
+    list_filter = ['vergeben']
--- eduroam/models.py
+++ eduroam/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class GuestAccount(models.Model):
+    """
+    repräsentiert einen eduroam Gastaccount, der vom Infopunkt verliehen
+    werden kann
+    """
+
+    # Account Stammdaten
+    login = models.CharField(max_length=100, verbose_name='Accountname')
+    password = models.CharField(max_length=100, verbose_name='Passwort')
+    gueltig_von = models.DateTimeField()
+    gueltig_bis = models.DateTimeField()
+
+    # Account Ausleihe
+    vergeben = models.BooleanField()
+    vorname = models.CharField(max_length=100, null=True, blank=True)
+    nachname = models.CharField(max_length=100, null=True, blank=True)
+    perso_id = models.CharField(max_length=100, verbose_name='Personalausweisnummer', null=True, blank=True)
+
+    # ausleihender Infopunkt-Engel
+    vergeben_am = models.DateTimeField(null=True, blank=True)
+    vergeben_durch = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        ordering = ('login',)
+        verbose_name = 'Gastaccount'
