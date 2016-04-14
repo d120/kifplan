bef599d6e1f8851f711c40cc40ccb0517389305a
Johannes Lauinger <johannes@lauinger-it.de>
2016-04-14 02:24:20 +0200

    blafoO

--- aks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from aks.models import AK
-
-@admin.register(AK)
-class AKAdmin(admin.ModelAdmin):
-    # admin list table view
-    list_display = ['titel', 'leiter', 'anzahl', 'wann', 'dauer']
-    list_display_links = ['titel']
