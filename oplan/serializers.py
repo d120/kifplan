8e8bbc3af7d6be96553a26df4233b718d84bbf4d
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2018-10-19 10:58:41 +0200

    Further compatibility fixes

--- oplan/serializers.py
+++ oplan/serializers.py
@@ -2,19 +2,29 @@ from rest_framework import serializers
 
 from oplan.models import AK, Room, RoomAvailability, AKTermin
 
+
 class AKSerializer(serializers.ModelSerializer):
     class Meta:
         model = AK
+        fields = '__all__'
+
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
+        fields = '__all__'
+
+
 class RoomAvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomAvailability
+        fields = '__all__'
+
+
 class AKTerminSerializer(serializers.ModelSerializer):
     class Meta:
         model = AKTermin
-        fields = ('ak_titel', 'ak_color', 'ak_constraints_freetext')
+        fields = '__all__'
 
     ak_titel = serializers.CharField(source='ak.titel', read_only=True)
     ak_color = serializers.CharField(source='ak.color', read_only=True)
